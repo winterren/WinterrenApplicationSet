c5243e521442f16c52fc181a9ce6e69fdaf6ce52
任远 <winterren@promote.cache-dns.local>
2016-06-01 19:17:17 +0800

    Initial commit

---
+++ app/src/main/java/com/winterhouse/winterren/winterrenapplicationset/MainActivity.java
@@ -0,0 +1,13 @@
+package com.winterhouse.winterren.winterrenapplicationset;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
